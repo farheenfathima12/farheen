7be0849d8543e9f9e5d6b3fd9e61fd521575a192
farheenfathima12 <farheenfathima86@gmail.com>
2023-02-23 17:35:37 +0530

    pushed

---
+++ Control.java
@@ -0,0 +1,22 @@
+package com.farhu;
+
+public class Control {
+	public static void main(String[] args) {
+		int i=20;
+		if(i==10) {
+			System.out.println("If");
+		}
+		else if (i==15) {
+			System.out.println("ElseIf");
+		}
+		
+		else {
+			System.out.println("Else");
+		}
+	
+		
+	}
+
+}
+
+
